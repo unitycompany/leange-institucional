f94d7789db33237f706464ecf2dbd2717bd48195
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-11-08 12:56:57 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+     rollupOptions: {
+       external: [
+          "leange/assets/index-BHhcukG4.js" 
+       ],
+     },
+  },
   plugins: [react()],
   base: "/leange/", 
 });
