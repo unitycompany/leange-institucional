12661b84b9b7ec0f531b695475c57f5c4797505a
unitycompany <equipe@unitycompany.com.br>
2025-01-23 16:20:35 -0300

    add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -770,7 +770,7 @@ const Home = () => {
                 <StyledEstruturaSubTitle data-aos="fade-up" data-aos-delay="200">Estamos te esperando!</StyledEstruturaSubTitle>
             </div>
 
-            <EventAlert id="pacotes"git />
+            <EventAlert id="pacotes" />
 
             <StyledContainerEvents data-aos="fade-in" data-aos-delay="250">
                 <EventCardCarousel events={events} />
