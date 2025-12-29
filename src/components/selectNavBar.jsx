5decb9df64f3b87e656fcb20d067a35894790b44
Aleph Ramos <equipe@unitycompany.com.br>
2025-12-29 13:10:02 -0300

    commit rápido

--- src/components/selectNavBar.jsx
+++ src/components/selectNavBar.jsx
@@ -99,7 +99,7 @@ const RedirectDropdown = () => {
   const options = [
     { label: "Acomodações Serra", path: "/acomodaSerra" },
     { label: "Acomodações Mar", path: "/acomodaMar" },
-    { label: "Faça o seu Evento", path: "/event" },
+    // { label: "Faça o seu Evento", path: "/event" },
   ];
 
   const handleSelect = (option) => {
